c81d737d8707e883ae79d9c5f72abc4fc2077dd7
Anushka20222 <anushka_201900410@smit.smu.edu.in>
2021-11-13 21:59:01 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SpaceXComponent } from './space-x/space-x.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'',component:TodoDetailsComponent},
+  {path:'list',component:TodoListComponent},
+  {path:'details',component:TodoDetailsComponent},
+  {path:'spaceX',component:SpaceXComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+import { TodoService } from './todo.service';
+import { SpaceXComponent } from './space-x/space-x.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDetailsComponent,
+    SpaceXComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
